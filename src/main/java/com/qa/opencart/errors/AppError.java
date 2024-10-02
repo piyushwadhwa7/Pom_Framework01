99e43e7156248bb1d040df6128ea9219309b55dd
Piyush Wadhwa <piyush.wadhwa.703@gmail.com>
2024-10-03 01:19:22 +0530

    Added test cases for login page with proper config files and Apperros and AppConstants

--- src/main/java/com/qa/opencart/errors/AppError.java
+++ src/main/java/com/qa/opencart/errors/AppError.java
@@ -1,4 +1,11 @@
 package com.qa.opencart.errors;
 
 public class AppError {
+    public static final String BROWSER_NOT_FOUND = "=======BROWSER NOT FOUDN========";
+
+    public static final String TITLE_NOT_FOUND = "=======TITLE NOT FOUDN========";
+
+    public static final String URL_NOT_FOUND = "=======URL NOT FOUDN========";
+
+    public static final String ELEMENT_NOT_FOUND = "=======ELEMENT NOT FOUDN========";
 }
